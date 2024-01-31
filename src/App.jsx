0879870841a0f75823ec1462a181210da6aa0e09
Devdhee <kvngdhavid@gmail.com>
2024-01-31 01:47:22 +0100

    sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,21 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import HomePage from './Pages/HomePage'
-import Classes from './Pages/Classes'
-import Services from './Pages/Services'
-import Layout from './Components/Layout'
-import SignUp from './Pages/SignUp'
-
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import Classes from "./Pages/Classes";
+import Services from "./Pages/Services";
+import Layout from "./Components/Layout";
+import SignUp from "./Pages/SignUp";
 
 export default function App() {
   return (
     <BrowserRouter>
-
-    <Routes>
-      <Route element = {<Layout/>}>
-
-      <Route path = '/' element = {<HomePage/>}/>
-      <Route path = '/classes' element = {<Classes/>}/>
-      <Route path = '/services' element = {<Services/>}/>
-      <Route path = '/signup' element = {<SignUp/>}/>
-
-      </Route>
-      
-    </Routes>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/classes" element={<Classes />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export const Button = ({children, type}) => {
-    const base = " text-base inline-block bg-green rounded-[100px] text-light focus:ring-[#98A2B3]"
+export const Button = ({ children, type, to }) => {
+  const base =
+    " text-base inline-block bg-green rounded-[100px] text-light focus:ring-[#98A2B3]";
 
-    const styles ={
-        primary: "py-2 px-6 text-base bg-light text-green inline-block rounded-[100px] border-solid border-2 border-green font-semibold hover:text-white hover:bg-green hover:border-white cursor-pointer",
-        small: base + " py-2 px-6 text-base border-solid border-2 border-light cursor-pointer",
-        medium: " bg-white py-2 px-6 text-base border-solid border-2 border-light"
-    }
-  return (
-    <div className={styles[type]}> {children}</div>
-  )
-}
+  const styles = {
+    primary:
+      "py-2 px-6 text-base bg-light text-green inline-block rounded-[100px] border-solid border-2 border-green font-semibold hover:text-white hover:bg-green hover:border-white cursor-pointer",
+    small:
+      base +
+      " py-2 px-6 text-base border-solid border-2 border-light cursor-pointer",
+    medium: " bg-white py-2 px-6 text-base border-solid border-2 border-light",
+  };
+
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+
+  return <div className={styles[type]}> {children}</div>;
+};
